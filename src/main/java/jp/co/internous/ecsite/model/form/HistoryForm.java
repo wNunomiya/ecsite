5d6eed8186057ae4f4fc24d4e3829244e2a86001
wNunomiya <kifxasw625@gmail.com>
2024-02-02 20:02:02 +0900

    ecsiteコミット

---
+++ src/main/java/jp/co/internous/ecsite/model/form/HistoryForm.java
@@ -0,0 +1,15 @@
+package jp.co.internous.ecsite.model.form;
+
+public class HistoryForm {
+	
+	private int userId;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+}
